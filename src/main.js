52b4c701b6713831ae695c8d66d09912eb20e66b
caozhen <caozhen@segimail.com>
2021-04-01 14:25:01 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+import VueExcelEditor from 'vue-excel-editor'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueExcel from 'vue-element-excel'
+
+Vue.use(Antd)
+Vue.use(VueExcelEditor)
+Vue.use(ElementUI)
+Vue.use(VueExcel)
+
 Vue.config.productionTip = false
 
 new Vue({
